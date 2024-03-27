8cafbb777128bb2b3c8551ec8ffff8359e551467
DanielJoel <danpersie53@gmail.com>
2024-03-27 20:47:06 +0300

    functions cont

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// code your solution here
+function saturdayFun(activity) {
+    if (activity === undefined) {
+        message = "This Saturday, I want to roller-skate!"
+
+    }
+    else {
+        message = `This Saturday, I want to ${activity}!`
+    }
+    return message
+}
+let message
+
+function mondayWork(msg = 'go to the office') {
+    const overwriteFun = function (msg) {
+        return `This Monday, I will ${msg}.`
+
+    }
+    return overwriteFun(msg)
+
+
+}
+function wrapAdjective(flair = "*") {
+    function innerFun(adjective) {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+    return innerFun
+
+}
+
+
+
